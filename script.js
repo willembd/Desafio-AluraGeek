73514079b4bcda97692521578216a7d6c54198b5
Willemberg Bruno <57848099+willembd@users.noreply.github.com>
2024-06-13 20:15:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const sNome = document.querySelector('#nome')
+const sValor = document.querySelector('#valor')
+const sImagem = document.querySelector('#imagem')
+const sButton_guarda = document.querySelector('#button_guarda')
+const sButton_limpar = document.querySelector('#button_limpar')
+
